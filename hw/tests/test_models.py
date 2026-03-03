e7e4f47a726457fde3b440fedb7377634b17c333
Myrka <novot129@fjfi.cvut.cz>
2026-03-03 16:55:19 +0100

    test update

--- hw/tests/test_models.py
+++ hw/tests/test_models.py
@@ -6,75 +6,11 @@ from django.utils import timezone
 from datetime import timedelta
 from django.core.exceptions import ValidationError
 
-'''class AssignmentValidationTest(BaseHWTestCase):
-    def test_deadline_before_release_is_invalid(self):
-        ass = Assignment(
-            title="Chybný úkol",
-            subject=self.subject,
-            teacher=self.teacher,
-            description="...",
-            max_score=5,
-            release=timezone.now(),
-            deadline=timezone.now() - timedelta(days=1),
-        )
-
-        with self.assertRaises(ValidationError):
-            ass.full_clean()
-            
-    def test_deadline_equal_release_is_valid(self):
-        now = timezone.now()
-        ass = Assignment(
-            title="OK úkol",
-            subject=self.subject,
-            teacher=self.teacher,
-            description="...",
-            max_score=10,
-            release=now,
-            deadline=now,
-        )
-        ass.full_clean()            
-
-    def test_score_within_limit_is_valid(self):
-        ev = Homework(
-          key=self.key,
-          engrossment = f"modelsbla",
-          submitted = timezone.now(),
-          text_evaluation= f"bla",
-          score=6,
-        )
-        ev.full_clean() 
-
-class HomeworkFlowTest(BaseHWTestCase):
-    def test_student_can_submit_homework(self):
-        hw = Homework.objects.create(
-            key=self.key,
-            score=8,
-            submitted=timezone.now()
-        )
-        self.assertEqual(hw.score, 8)
-        self.assertEqual(hw.key.student, self.student)
-
-class ScoreEvaluationTest(BaseHWTestCase):
-    def test_score_maxscore(self):
-          ev=Homework(
-          key=self.key,
-          engrossment = f"modelsbla",
-          submitted = timezone.now(),
-          text_evaluation= f"bla",
-          score=60,
-          )
-          with self.assertRaises(ValidationError):
-               ev.full_clean()
-
-class ModelStrTest(BaseHWTestCase):
-    def test_assignment_str(self):
-        self.assertEqual(str(self.assignment), "Domácí úkol 1")
-'''
 class TestSubject(TestCase):
     def setUp(self):
         self.subject=Subject(year=1980, name="INTA")
     def test_to_str(self):
-        self.assertEqual(str(self.subject), "1980-INTA")
+        self.assertEqual(str(self.subject), "INTA-1980")
         
 class TestAssignment(BaseHWTestCase):
     def test_to_str(self):
@@ -87,7 +23,7 @@ class TestAssignment(BaseHWTestCase):
 
 class TestKey(BaseHWTestCase):
     def test_to_str(self):
-        self.assertEqual(str(self.key),"student-Domácí úkol 1")   
+        self.assertEqual(str(self.key),"Adam Břídil-Domácí úkol 1")   
     def test_validate_subject_exists(self):
         self.assignment.subject=None
         self.assertEqual(self.assignment.subject,None)
