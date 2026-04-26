ccfa64cbb1a7faad90761ccbc3e407151292c08f
unknown <novot129@fjfi.cvut.cz>
2026-04-26 21:50:01 +0200

    tst

--- hw/tests/test_views_correct.py
+++ hw/tests/test_views_correct.py
@@ -73,4 +73,106 @@ class TestPages(BaseHWTestCase):
         # Zkontrolujeme, že se v messages objevila chyba
         messages = list(response.wsgi_request._messages)
         self.assertTrue(any("Po deadline" in str(m) for m in messages))
-      
+
+class HWViewStatus200Tests(BaseHWTestCase):
+
+    def assert_get_200(self, user, url_name, **kwargs):
+        self.client.force_login(user)
+        response = self.client.get(reverse(url_name, kwargs=kwargs))
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_active_200(self):
+        self.assert_get_200(self.student, "list_active")
+
+    def test_list_after_deadline_200(self):
+        self.assert_get_200(self.student, "list_after_deadline")
+
+    def test_list_before_release_200_teacher(self):
+        self.assert_get_200(self.teacher, "list_before_release")
+
+    def test_assignment_teacher_detail_200(self):
+        self.assert_get_200(
+            self.teacher,
+            "assgn_detail_teacher",
+            pk=self.assignment.pk,
+        )
+
+    def test_assignment_student_detail_200(self):
+        self.assert_get_200(
+            self.student,
+            "assgn_detail_student",
+            pk=self.assignment.pk,
+        )
+
+    def test_assignment_create_200(self):
+        self.assert_get_200(self.teacher, "assignment_create")
+
+    def test_homework_submit_200(self):
+        self.client.force_login(self.student)
+        response = self.client.get(
+            reverse("hw_submit"),
+            {"assgn_id": self.assignment2.pk},
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_homework_update_200(self):
+        self.assert_get_200(
+            self.student,
+            "homework_update",
+            pk=self.homework.pk,
+        )
+
+    def test_assignment_delete_confirm_200(self):
+        self.assert_get_200(
+            self.teacher,
+            "assignment_delete",
+            pk=self.assignment.pk,
+        )
+
+    def test_homework_detail_student_200(self):
+        self.assert_get_200(
+            self.student,
+            "hw_detail",
+            pk=self.homework.pk,
+        )
+
+    def test_homework_detail_teacher_200(self):
+        self.assert_get_200(
+            self.teacher,
+            "hw_detail",
+            pk=self.homework.pk,
+        )
+
+    def test_evaluation_edit_200(self):
+        self.assert_get_200(
+            self.teacher,
+            "evaluate_edit",
+            pk=self.homework.pk,
+        )
+
+    def test_evaluation_delete_200(self):
+        self.assert_get_200(
+            self.teacher,
+            "evaluate_delete",
+            pk=self.homework.pk,
+        )
+
+    def test_student_comment_list_200(self):
+        self.assert_get_200(self.student, "student_comment_list")
+
+    def test_student_comment_detail_200(self):
+        self.assert_get_200(
+            self.student,
+            "student_comment_detail",
+            pk=self.studentComment2.pk,
+        )
+
+    def test_received_comment_detail_200(self):
+        self.assert_get_200(
+            self.student,
+            "student_received_comment_detail",
+            pk=self.studentComment.pk,
+        )
+
+    def test_teacher_comments_list_200(self):
+        self.assert_get_200(self.teacher, "teacher_comments_list")
